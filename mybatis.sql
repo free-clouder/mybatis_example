0939ce70de337fe680d293298e08a5043de26a22
ice_in_summer <2053620282@qq.com>
2022-03-27 01:47:17 +0800

    add .sql

---
+++ mybatis.sql
@@ -0,0 +1,11 @@
+drop DATABASE mybatis;
+create DATABASE mybatis;
+use mybatis;
+create table t_user(
+    id int primary key auto_increment,
+    username varchar(255),
+    password varchar(255),
+    age int,
+    sex char,
+    email varchar(255)
+)
